bbb0f52388a35064724c28ef2dcc90acee417858
user-daw-zayas <javier.perezarteaga@educa.madrid.org>
2015-03-17 13:21:25 +0100

    openshift updating

--- server/config/openshift.js
+++ server/config/openshift.js
@@ -9,9 +9,9 @@ module.exports = {
 	},
 	ids: {
 		google: {
-			client_id:'380454543382-fqmk59spk85djt38boca89rub00nl44t.apps.googleusercontent.com',
-			client_secret: 'VThoqqMepxiVtwwk54--B-M5',
-			callback_url: 'http://socialscoreboard-dawzayas.rhcloud.com/auth/oauth2callback',
+			client_id:'447322105852-2ho65ghr9amnok0hqums9007944nfaa7.apps.googleusercontent.com',
+			client_secret: 'kwl9TtcsOJSnk39FN2sYW6R0',
+			callback_url: 'http://socialscoreboard-alexandra123456.rhcloud.com/auth/oauth2callback',
 			scopes: ['https://www.googleapis.com/auth/userinfo.profile']
 		}
 	},
